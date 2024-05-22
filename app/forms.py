5b3b32b65213b706b6ccd1d75e0067e7d50ec31b
Luan Esposito <luanesposito2003@gmail.com>
2024-05-22 20:10:47 -0300

    update forms

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired
 
 class CadastroFazendaForm(FlaskForm):
     nome = StringField(name='Nome', validators=[DataRequired()])
